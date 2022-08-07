56736c2d2fafbdac29ccbc5c0df6d7c9f864bed8
Jorge <jofer86@gmail.com>
2022-08-06 21:26:13 -0500

    re factor controller for more readability

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -5,6 +5,10 @@ const Transaction = require('../models/Transaction');
 const ErrorResponse = require('../utils/errorResponse.utils');
 const asyncHandler = require('../middleware/async.middleware');
 
+const throwError = (msg, code) => {
+  return new ErrorResponse(msg, code);
+};
+
 // @desc Get all Users
 // @route GET /api/v1/Users
 // @access Public
@@ -17,109 +21,13 @@ exports.getUsers = asyncHandler(async (req, res, next) => {
   });
 });
 
-// @desc Get the users Bank
-// @route GET /api/v1/Users/:id/bank
-// @access Public
-exports.getUserBank = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.params.id);
-  if (!user) {
-    return next(new ErrorResponse(`User not found with id of ${req.params.id}`, 404));
-  }
-  const bank = await Bank.findOne({ user: user._id });
-  res.status(200).json({
-    success: true,
-    data: bank
-  });
-});
-
-// @desc add an expense to the users bank
-// @route POST /api/v1/Users/:id/add_transaction
-// @access Public
-exports.addTransaction = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.params.id);
-
-  if (!user) {
-    return next(new ErrorResponse(`User not found with id of ${req.params.id}`, 404));
-  }
-  const bank = await Bank.findOne({ user: req.params.id });
-  if (!bank) {
-    return next(new ErrorResponse(`Bank not found with id of ${req.params.id}`, 404));
-  }
-  const expenseMonth = await ExpenseMonth.findById(req.body.expense_month_id);
-  if (!expenseMonth) {
-    return next(new ErrorResponse(`Expense Month not found with id of ${req.params.id}`, 404));
-  }
-
-  const transaction = new Transaction({
-    description: req.body.description,
-    amount: req.body.amount,
-    transactionType: req.body.transactionType,
-    bank: bank._id,
-    user: user._id,
-    expenseMonth: expenseMonth._id
-  });
-
-  if (transaction.transactionType === 'expense') {
-    bank.totalBalance -= transaction.amount;
-    bank.totalExpense += transaction.amount;
-    expenseMonth.totalExpense += transaction.amount;
-    expenseMonth.totalBalance -= transaction.amount;
-  }
-
-  if (transaction.transactionType === 'income') {
-    bank.totalBalance += transaction.amount;
-    bank.totalIncome += transaction.amount;
-    expenseMonth.totalIncome += transaction.amount;
-    expenseMonth.totalBalance += transaction.amount;
-  }
-
-  try {
-    await expenseMonth.save();
-    await transaction.save();
-    await bank.save();
-    res.status(200).json({
-      success: true,
-      data: {
-        user,
-        bank,
-        transaction,
-        expenseMonth
-      }
-    });
-  } catch {
-    res.status(500).json({
-      success: false,
-      message: 'Server Error'
-    });
-  }
-});
-
-// @desc create an expense month.
-// @route POST /api/v1/Users/:id/create_expense_month
-// @access Public
-exports.createExpenseMonth = asyncHandler(async (req, res, next) => {
-  const expenseMonth = new ExpenseMonth({
-    name: req.body.name
-  });
-
-  if (expenseMonth.save()) {
-    res.status(200).json({
-      success: true,
-      data: expenseMonth
-    });
-  }
-
-  return next(new ErrorResponse(`Something aint right`, 500));
-});
-
 // @desc Get single botcamp
 // @route GET /api/v1/Users/:id
 // @access Public
 exports.getUser = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.params.id);
-  if (!user) {
-    return next(new ErrorResponse(`User not found with id of ${req.params.id}`, 404));
-  }
+  if (!user) return next(throwError('User not found', 404));
+
   res.status(200).json({
     success: true,
     data: user
@@ -146,9 +54,8 @@ exports.updateUser = asyncHandler(async (req, res, next) => {
     new: true,
     runValidators: true
   });
-  if (!user) {
-    return next(new ErrorResponse('User not found', 404));
-  }
+  if (!user) return next(throwError('User not found', 404));
+
   res.status(200).json({
     success: true,
     data: user
@@ -160,11 +67,98 @@ exports.updateUser = asyncHandler(async (req, res, next) => {
 // @access Private
 exports.deleteUser = asyncHandler(async (req, res, next) => {
   const user = await User.findByIdAndDelete(req.params.id);
-  if (!user) {
-    return next(new ErrorResponse('User not found', 404));
-  }
+  if (!user) return next(throwError('User not found', 404));
+
   res.status(200).json({
     success: true,
     data: user
   });
+});
+
+// @desc Get the users Bank
+// @route GET /api/v1/Users/:id/bank
+// @access Public
+exports.getUserBank = asyncHandler(async (req, res, next) => {
+  const bank = await Bank.findOne({ user: req.params.id });
+  if (!bank) return next(throwError('User bank not found', 404));
+
+  res.status(200).json({
+    success: true,
+    data: bank
+  });
+});
+
+// @desc add an expense to the users bank
+// @route POST /api/v1/Users/:id/add_transaction
+// @access Public
+exports.addTransaction = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  if (!user) return throwError(`User not found with id of ${req.params.id}`, 404);
+
+  const bank = await Bank.findOne({ user: req.params.id });
+  if (!bank) return throwError(`No bank found for user with id of ${req.params.id}`, 404);
+
+  const expenseMonth = await ExpenseMonth.findById(req.body.expense_month_id);
+  if (!expenseMonth) return throwError(`Expense month not found`, 404);
+
+  const transaction = new Transaction({
+    description: req.body.description,
+    amount: req.body.amount,
+    transactionType: req.body.transactionType,
+    bank: bank._id,
+    user: user._id,
+    expenseMonth: expenseMonth._id
+  });
+
+  if (transaction.transactionType === 'expense') {
+    bank.totalBalance -= transaction.amount;
+    bank.totalExpense += transaction.amount;
+    expenseMonth.totalExpense += transaction.amount;
+    expenseMonth.totalBalance -= transaction.amount;
+  }
+
+  if (transaction.transactionType === 'income') {
+    bank.totalBalance += transaction.amount;
+    bank.totalIncome += transaction.amount;
+    expenseMonth.totalIncome += transaction.amount;
+    expenseMonth.totalBalance += transaction.amount;
+  }
+
+  try {
+    await bank.save();
+    await expenseMonth.save();
+    await transaction.save();
+    res.status(200).json({
+      success: true,
+      data: {
+        user,
+        bank,
+        transaction,
+        expenseMonth
+      }
+    });
+  } catch {
+    res.status(500).json({
+      success: false,
+      message: 'Server Error'
+    });
+  }
+});
+
+// @desc create an expense month.
+// @route POST /api/v1/Users/:id/create_expense_month
+// @access Public
+exports.createExpenseMonth = asyncHandler(async (req, res, next) => {
+  const expenseMonth = new ExpenseMonth({
+    name: req.body.name
+  });
+
+  if (expenseMonth.save()) {
+    return res.status(200).json({
+      success: true,
+      data: expenseMonth
+    });
+  }
+
+  return next(new ErrorResponse(`Something aint right`, 500));
 });
